52cc540fa2aa060b1730c8b9296883b4b6b5e708
Relix <gerribarzdainis@gmail.com>
2026-05-19 21:25:45 +0200

    Delete NyroxHub directory

--- NyroxHub/Games/Universal/main.lua
+++
@@ -1,8 +0,0 @@
--- c:/Users/lukeb/OneDrive/Projects/Original/Scripts/NyroxHub/Games/Universal/main.lua
-local Import = getgenv().Import
-
--- 1. Lade die UI Konfiguration
-Import("Games/Universal/Ui/window_config.lua")
--- 2. Hier kommt die Spiellogik hin
--- Hier könntest du deine Loops starten:
--- task.spawn(function() while _G.Farm do task.wait() ... end end)
